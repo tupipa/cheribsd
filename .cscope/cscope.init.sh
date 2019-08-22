c9e9a3ebb894d4e6c0108f254f681ae89e0e53a2
Lele Ma <lma17@ur.rochester.edu>
2019-08-22 10:55:49 -0400

    add cscope script

---
+++ .cscope/cscope.init.sh
@@ -0,0 +1,25 @@
+
+HOME=$HOME
+
+SRC_ROOT=$HOME/lab/sva/cheri/cheribsd
+
+cd / 	
+find  $SRC_ROOT                                                                \
+	-path "$SRC_ROOT/include/asm-*" ! -path "$SRC_ROOT/include/asm-i386*" -prune -o \
+	-path "$SRC_ROOT/sys/arm*" -prune -o             \
+	-path "$SRC_ROOT/sys/arm64*" -prune -o             \
+	-path "$SRC_ROOT/sys/i386*" -prune -o             \
+	-path "$SRC_ROOT/sys/powerpc*" -prune -o             \
+	-path "$SRC_ROOT/sys/sparc64*" -prune -o             \
+	-path "$SRC_ROOT/contrib*" -prune -o             \
+        -name "*.[chxsS]" -print >$SRC_ROOT/.cscope/cscope.files
+
+
+#	-path "$SRC_ROOT/sys/riscv*" -prune -o             \
+#	-path "$SRC_ROOT/sys/amd64*" -prune -o             \
+
+
+# regenerate the database
+
+cd $SRC_ROOT/.cscope && cscope -b -q -k
+
